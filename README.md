1822ea0c1e5d25e75029db168e671761c33fb828
luanlpg <luanlpg13@gmail.com>
2019-05-15 00:17:02 -0300

    main

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# zina
+# ZINA
+
+
+## Instalação de requisitos (NECESSÁRIO:Python 3.6.0+
+
+- Faça um clone do projeto: `git clone https://github.com/Luanlpg/zina.git`
+
+- Acesse o repositório: `cd zina/`
+
+- não foi adicionado o arquivo `requirements.txt` por não ser utilizada nenhuma lib externa
+
+## Rodando script
+
+- Rode o script com: `python start`
+
+- Aguarde a execução de tds os processos...
--- start/__main__.py
+++ start/__main__.py
@@ -1,31 +1,44 @@
 from core import is_bouncy
 
 def is_99_percent(num):
+    '''=========================================================================
+    Método que recebe um numero e determina se os 99% foram atingidos.
+    ========================================================================='''
     bouncy = 0
+    # para cada numero
     for i in range(num):
+        # verifico se é bouncy
         if is_bouncy(i):
+            # se for adc um em bouncy
             bouncy += 1
+    # verifico pocentagem de bouncy
     percent = 100 - ((num - bouncy) / bouncy * 100)
+    # se for maior ou igual a 99
     if percent >= 99:
-        print('passou')
+        # retorno True
         return True, percent
     return False, percent
 
 init = 21780
 interval = 1000000
 result = 0
+
+# itero por 7 vezes para melhorar performance
 for i in range(7):
+    # itero com intervalos e numeros iniciais diferentes
     for i in range(init, 9999999999, interval):
         bool, percent = is_99_percent(i)
+        # caso passe de 99%
         if bool == True:
-            print(percent, i)
+            print(percent,'% - ', i)
             result = i
-            print('break')
+            # paro loop
             break
-        print(percent, i)
+        print(percent,'% - ', i)
+    # set variavel inicial e de intervalo
     init = result - interval
     interval = int(interval/10)
-
+# retorno resultado
 print('==============================================')
 print(f'================={result}======================')
 print('==============================================')
